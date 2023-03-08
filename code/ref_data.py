4065e7cdd132fb39432db8e0883cbfe520bc088d
Freya Porter <freyaporter@kubrick.com>
2023-03-08 18:21:54 +0000

    started part3B

--- code/ref_data.py
+++ code/ref_data.py
@@ -1,6 +1,11 @@
 
 import csv
 import pandas as pd
+import yahoofinancials
+from yahoofinancials import YahooFinancials
+
+
+
 csv_file = 'sp-100-index-03-07-2023.csv'
 def get_sp100():
     # sp100 = []
@@ -12,3 +17,15 @@ def get_sp100():
     df =pd.read_csv(csv_file)
     
     return list(df['Symbol'])
+
+
+
+def get_yahoo_data(start_date,end_date,tickers):
+
+
+    for ticker in tickers:
+        data = YahooFinancials(ticker).get_historical_price_data(start_date, end_date, 'daily')
+        prices = pd.DataFrame(data[ticker]['prices'])
+        prices['1daily_return'] = prices['open'] - prices['close'].shift(1)
+        return prices
+
